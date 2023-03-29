2c68faa49e0451daa0c4c738f4138e7355472c6c
Vasantharaja <98202074+vasantharaja200295@users.noreply.github.com>
2023-03-29 22:38:23 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+import pygame
+import sys
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def game_floor():
+    screen.blit(floor_base, (floor_x_pos,900))
+
+pygame.init()
+clock = pygame.time.Clock()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+screen = pygame.display.set_mode((570, 1024))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+background = pygame.image.load('./assets/background.png').convert()
+background = pygame.transform.scale2x(background)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+bird = pygame.image.load('./assets/yellowbird-midflap.png').convert_alpha()
+bird_rect = bird.get_rect(center=(100, 512))
+bird = pygame.transform.scale2x(bird)
+
+bird_y = 0
+gravity = 0.1
+
+
+floor_base = pygame.image.load('./assets/ground.png').convert()
+floor_base = pygame.transform.scale2x(floor_base)
+floor_x_pos = 0
+
+
+while True:
+
+    screen.blit(background, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_y = 0
+                bird_y -= 0.01
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                bird_y = 0
+                bird_y +=2
+
+    bird_y += gravity
+    bird_rect.centery += bird_y
+
+    screen.blit(bird, bird_rect)
+
+    floor_x_pos -= 1
+    game_floor()
+    if floor_x_pos <= -76:
+        floor_x_pos = 0
+
+    pygame.display.update()
+    clock.tick(120)
+
+
+
+
